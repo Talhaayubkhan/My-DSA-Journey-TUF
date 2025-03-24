fb24a177505741fae264b246b0fcb23e3499ea71
Talha Ayub <103215272+Talhaayubkhan@users.noreply.github.com>
2025-03-25 00:04:39 +0500

    update the patterns section with newly added

--- Logic Building/Patterns.java
+++ Logic Building/Patterns.java
@@ -97,24 +97,74 @@ public class Patterns {
       }
 
       public static void patternsPrint8(int n) {
-
-            for (int i = 1; i <= 2 * n - 1; i++) {
-
+            for (int i = 1; i <= 2 * n - 1; i++) { // Loop runs from 1 to (2 * n - 1)
                   int stars = i;
-                  if (i > n)
+                  if (i > n) { // If i exceeds n, adjust stars
                         stars = 2 * n - i;
-
-                  for (int j = 1; j <= stars; j++) {
+                  }
+                  for (int j = 1; j <= stars; j++) { // Print 'stars' number of '*'
                         System.out.print("* ");
+                  }
+                  System.out.println(""); // Move to the next line
+            }
+      }
+
+      public static void patternsPrint9(int n) {
+            int start = 1;
+            for (int i = 0; i < n; i++) {
+
+                  if (i % 2 == 0) {
+                        start = 1;
+                  } else {
+                        start = 0;
+                  }
+
+                  for (int j = 0; j <= i; j++) {
+                        System.out.print(start);
+                        start = 1 - start;
                   }
                   System.out.println();
             }
 
       }
 
+      public static void patternsPrint10(int n) {
+
+            int spaces = 2 * (n - 1);
+            // outer loop
+            for (int i = 1; i <= n; i++) {
+                  // numbers
+                  for (int j = 1; j <= i; j++) {
+                        System.out.print(j);
+                  }
+                  // space
+                  for (int j = 1; j < spaces; j++) {
+                        System.out.print(" ");
+                  }
+                  // numbers in opposite
+                  for (int j = i; j >= 1; j--) {
+                        System.out.print(j);
+                  }
+                  System.out.println();
+                  spaces -= 2;
+            }
+
+      }
+
+      public static void patternsPrint11(int n) {
+
+            int num = 1;
+
+            for (int i = 0; i < n; i++) {
+
+                  for (int j = 0; j < i; j++) {
+
+                  }
+            }
+      }
+
       public static void main(String[] args) {
             int n = 4;
-            patternsPrint8(n);
             // patternsPrint(n);
             // patternsPrint1(n);
             // patternsPrint2(n);
@@ -123,5 +173,8 @@ public class Patterns {
             // patternsPrint5(n);
             // patternsPrint6(n);
             // patternsPrint7(n);
+            // patternsPrint8(n);
+            // patternsPrint9(n);
+            patternsPrint10(n);
       }
 }
